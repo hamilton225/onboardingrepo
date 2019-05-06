5f7a2593d2630ecc735dda21507af884e2668de4
hamilton225 <yao.hamilton95@gmail.com>
2019-05-06 17:58:10 +0200

    Adding route table

--- vpc/main.tf
+++ vpc/main.tf
@@ -12,10 +12,11 @@ resource "aws_vpc" "ob-vpc" {
     Name = "ob-test-vpc"
   }
 }
+
 #public subnet
 resource "aws_subnet" "public-subnet" {
-  vpc_id = "${aws_vpc.ob-vpc.id}"
-  cidr_block = "${var.public_subnet_cidr}"
+  vpc_id            = "${aws_vpc.ob-vpc.id}"
+  cidr_block        = "${var.public_subnet_cidr}"
   availability_zone = "eu-west-1a"
 
   tags {
@@ -25,11 +26,34 @@ resource "aws_subnet" "public-subnet" {
 
 #private subnet
 resource "aws_subnet" "private-subnet" {
-  vpc_id = "${aws_vpc.ob-vpc.id}"
-  cidr_block = "${var.private_subnet_cidr}"
+  vpc_id            = "${aws_vpc.ob-vpc.id}"
+  cidr_block        = "${var.private_subnet_cidr}"
   availability_zone = "eu-west-1b"
 
   tags {
     Name = "OB-VPC Private Subnet"
   }
 }
+
+# Define the route table
+resource "aws_route_table" "web-public-rt" {
+  vpc_id = "${aws_vpc.ob-vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.gw.id}"
+  }
+
+  tags {
+    Name = "Public Subnet RT"
+  }
+}
+
+#assign route table to the public Subnet
+resource "aws_route_table_association" "ob-public-rt" {
+  subnet_id = "${aws_subnet.public-subnet.id}"
+  route_table_id = "${aws_route_table.web-public-rt.id}"
+}
+
+
+
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -10,10 +10,10 @@ variable "vpc_cidr" {
 
 variable "public_subnet_cidr" {
   description = "CIDR for the public subnet"
-  default = "172.23.1.0/24"
+  default     = "172.23.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR for the private subnet"
-  default = "172.23.10.0/24"
+  default     = "172.23.10.0/24"
 }
