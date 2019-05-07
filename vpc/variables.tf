466fc8ceee4207e37b33ea34bb0e5bdcaeaa1e96
hamilton225 <yao.hamilton95@gmail.com>
2019-05-07 16:21:17 +0200

    Adding list variable for azs and subnets

--- vpc/variables.tf
+++ vpc/variables.tf
@@ -6,10 +6,14 @@ variable "vpc_cidr" {
   description = "CIDR for the VPC"
 }
 
-variable "public_subnet_cidr" {
-  description = "CIDR for the public subnet"
+variable "azs" {
+  type = "list"
 }
 
-variable "private_subnet_cidr" {
-  description = "CIDR for the private subnet"
+variable "public_subnets_cidr" {
+  type = "list"
+}
+
+variable "private_subnets_cidr" {
+  type = "list"
 }
