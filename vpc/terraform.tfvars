be06aceccec4cc78d0ecc1d226bb13ddfb8ae111
hamilton225 <yao.hamilton95@gmail.com>
2019-05-07 14:42:29 +0200

    Add ofvariable in  terraform.tfvars

---
+++ vpc/terraform.tfvars
@@ -0,0 +1,4 @@
+aws_region  = "eu-west-1"
+vpc_cidr  = "172.23.0.0/16"
+public_subnet_cidr = "172.23.1.0/24"
+private_subnet_cidr = "172.23.10.0/24"
--- vpc/variables.tf
+++ vpc/variables.tf
@@ -1,19 +1,15 @@
 variable "aws_region" {
   description = "Region for the VPC"
-  default     = "eu-west-1"
 }
 
 variable "vpc_cidr" {
   description = "CIDR for the VPC"
-  default     = "172.23.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "CIDR for the public subnet"
-  default     = "172.23.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "CIDR for the private subnet"
-  default     = "172.23.10.0/24"
 }
